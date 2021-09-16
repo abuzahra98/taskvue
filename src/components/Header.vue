822351d16b813360beccb6834bc5460653708727
abuzahra98 <dawoodbasher@gmail.com>
2021-09-16 22:03:03 +0300

    edit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+      <header>
+        <h1>Task</h1>
+    </header>
+</template>
+
+<script>
+export default {
+Name : 'Header',
+}
+</script>
+
+<style  scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+</style>
+
